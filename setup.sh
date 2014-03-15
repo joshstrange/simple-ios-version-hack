4eb44314f608604f2e9382f1114d032966c9b387
Josh Strange <josh@joshstrange.com>
2014-03-15 18:32:13 -0400

    Fixing urls and adding insecure curl option

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 cp /System/Library/CoreServices/SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist.original
 
 #Download SystemVersion files
-curl https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.4 -o /System/Library/CoreServices/SystemVersion.plist.7.0.4
-curl https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.6 -o /System/Library/CoreServices/SystemVersion.plist.7.0.6
+curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.4 -o /System/Library/CoreServices/SystemVersion.plist.7.0.4
+curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.6 -o /System/Library/CoreServices/SystemVersion.plist.7.0.6
 
 #Download scripts to switch SystemVersion files
-curl http://localhost:8000/switch-to-7.0.4 -o ~/switch-to-7.0.4
-curl http://localhost:8000/switch-to-7.0.6 -o ~/switch-to-7.0.6
+curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.4 -o ~/switch-to-7.0.4
+curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.6 -o ~/switch-to-7.0.6
 
 
