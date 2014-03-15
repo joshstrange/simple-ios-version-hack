2ba16f1c4ee45f0ee1047be57fe670a4cbb88a09
Josh Strange <josh@joshstrange.com>
2014-03-15 18:40:46 -0400

    Adding permission setting and silent options

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#Backup original file
 echo "Backing up original SystemVersion.plist to /System/Library/CoreServices/SystemVersion.plist.original"
 cp /System/Library/CoreServices/SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist.original
 
-#Download SystemVersion files
 echo "Downloading 7.0.4 and 7.0.6 versions of SystemVersion"
-curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.4 -o /System/Library/CoreServices/SystemVersion.plist.7.0.4
-curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.6 -o /System/Library/CoreServices/SystemVersion.plist.7.0.6
+curl --silent --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.4 -o /System/Library/CoreServices/SystemVersion.plist.7.0.4
+curl --silent --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/SystemVersion.plist.7.0.6 -o /System/Library/CoreServices/SystemVersion.plist.7.0.6
 
-#Download scripts to switch SystemVersion files
 echo "Downloading scripts to switch SystemVersion files"
-curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.4 -o ~/switch-to-7.0.4
-curl --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.6 -o ~/switch-to-7.0.6
+curl --silent --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.4 -o ~/switch-to-7.0.4
+curl --silent --insecure https://raw.github.com/joshstrange/simple-ios-version-hack/master/switch-to-7.0.6 -o ~/switch-to-7.0.6
 
-echo "Done! You can now use the commands: switch-to-7.0.4 and switch-to-7.0.6"
+echo "Setting execute permissions"
+chmod u+x ~/switch-to-7.0.4
+chmod u+x ~/switch-to-7.0.6
+
+echo "Done! You can now use the commands: ./switch-to-7.0.4 and ./switch-to-7.0.6"
